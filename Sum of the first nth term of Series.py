890c6c795987bf5a8802d0f5735c6ffecba56386
1waytocode <32342282+1waytocode@users.noreply.github.com>
2017-10-28 11:09:29 +0300

    Create Sum of the first nth term of Series.py

---
+++ Sum of the first nth term of Series.py
@@ -0,0 +1,8 @@
+Sum of the first nth term of Series
+https://www.codewars.com/kata/555eded1ad94b00403000071
+
+def series_sum(n):
+    sum = 0.0
+    for i in range(0,n):
+        sum += 1 / (1 + 3 * float(i))
+    return '%.2f' % sum
